a065a01bf3160356b9f7026eb26be63d6157b960
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-10-26 18:39:03 +0600

    second commit

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Main = () => {
     return (
         <div>
-            <h1>this is main</h1>
+            <h1>this is main </h1>
+            <h1>hey buddy</h1>
         </div>
     );
 };
